9a1a7b984509eec7f80ae7d2c688520267eb9c80
Alex0622 <aleximora2206@gmail.com>
2022-06-27 21:11:13 -0600

    Actualizar recetas recientes

--- js/recetasRecientes.js
+++ js/recetasRecientes.js
@@ -1,6 +1,16 @@
 const crearRecetasRecientes = () => {
     let recetasRecientes = document.querySelector(".recetasRecientes");
     recetasRecientes.innerHTML = `
+	<li>
+		<h4><a href="tostadas-de-pizza.html">Tostadas de pizza</a></h4>
+		<a href="tostadas-de-pizza.html"><img src="../images/tostadas-de-pizza.png" alt="" /></a>
+		<div class="price-details">
+			<div class="add-cart">								
+				<h4><a href="tostadas-de-pizza.html">Ver receta</a></h4>
+		    </div>
+			<div class="clear"></div>
+    	</div>					 
+	</li>
     <li>
 		<h4><a href="tortas-de-carne.html">Tortas de carne</a></h4>
 		<a href="tortas-de-carne.html"><img src="../images/tortas-de-carne.png" alt="" /></a>
@@ -10,16 +20,6 @@ const crearRecetasRecientes = () => {
 			</div>
 	    	<div class="clear"></div>
 		</div>					 
-	</li>
-    <li>
-		<h4><a href="chirmol.html">Chirmol</a></h4>
-		<a href="chirmol.html"><img src="../images/chirmol-1.png" alt="" /></a>
-		<div class="price-details">
-			<div class="add-cart">								
-				<h4><a href="chirmol.html">Ver receta</a></h4>
-		    </div>
-			<div class="clear"></div>
-    	</div>					 
 	</li>
     `;
 
